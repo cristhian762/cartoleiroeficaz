683d46946b780b0090631828ece3c5fb5b801c70
Vinicius Da Hora Corte <20181BSI0105@cefetes.br>
2022-06-14 08:01:01 -0300

    utils

--- backend/routes.js
+++ backend/routes.js
@@ -1,13 +1,13 @@
 const express = require('express')
 
 const StatusMercadoController = require('./controllers/StatusMercadoController')
-const EscalacaoController = require('./controllers/StatusMercadoController')
+const EscalacaoController = require('./controllers/EscalacaoController')
 const CustoMinimoController = require('./controllers/CustoMinimoController')
 
 const routes = express.Router()
 
 routes.get('/escalacao', EscalacaoController.generate)
 routes.get('/statusMercado', StatusMercadoController.index)
-routes.get('/custoMinimo', CustoMinimoController.index)
+routes.get('/custoMinimo', CustoMinimoController.generate)
 
 module.exports = routes
