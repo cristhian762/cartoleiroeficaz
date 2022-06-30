0a4126ec4d70e6bffce00c57e5989544c4e68402
viniciushora <viniciuscorte@outlook.com>
2022-06-30 00:58:16 -0300

    backend: controllers

--- backend/controllers/CustoMinimoController.js
+++ backend/controllers/CustoMinimoController.js
@@ -1,16 +1,36 @@
 const axios = require('axios');
-import {Vertice} from '../utils/classes/Vertice'
+import {Jogador} from '../utils/classes/Jogador'
+import {geraGrafo} from './geraGrafo'
+import {geraCustoMinimo} from './geraCustoMinimo'
 
 module.exports = {
 
   async generate (request, response) {
     const apiResponse = await axios.get('https://api.cartola.globo.com/atletas/mercado')
     const { formacao } = request.body
+    var jogadores = []
 
-    for(var i=0; i<apiResponse.atletas.length; i++){
-      
+    var atletas = apiResponse.atletas
+    var clubes = apiResponse.clubes
+    for(var i=0; i<atletas.length; i++){
+      var jogador = new Jogador(
+        atletas[i].atleta_id,
+        atletas[i].apelido,
+        atletas[i].status_id,
+        atletas[i].foto,
+        clubes[atletas[i].atleta_id].escudos["60x60"],
+        atletas[i].posicao_id,
+        atletas[i].preco_num,
+        atletas[i].media_num,
+        atletas[i].minimo_para_valorizar
+      )
+      jogadores.add(jogador)
     }
 
-    return response.json({ apiResponse })
+    var g = geraGrafo(jogadores,1)
+
+    var custoMinimo = geraCustoMinimo(g, formacao)
+
+    return response.json({ custoMinimo })
   }
 }
--- backend/utils/functions/geraCustoMinimo.js
+++ backend/utils/functions/geraCustoMinimo.js
@@ -8,6 +8,7 @@ function geraCustoMinimo(jogadores, formacao){
     var lista_jogadores = []
     var g = geraGrafo(jogadores, 1)
     var arvore = kruskal(g, formacao)
+    var custoMinimo = 0
 
     for(var i=0; i<jogadores.length; i++){
         var v = arvore.get(jogadores[i].id).pred
@@ -20,10 +21,11 @@ function geraCustoMinimo(jogadores, formacao){
     var l1 = preds.concat(suces);
     const ids_jogadores = [...l1]
 
+
     for(var j=0;j<ids_jogadores.length;j++){
         var jogador = jogadores.getJogador(ids_jogadores[j])
-        lista_jogadores.add(jogador)
+        custoMinimo = custoMinimo + jogador.preco
     }
 
-    return lista_jogadores
+    return custoMinimo
 }
